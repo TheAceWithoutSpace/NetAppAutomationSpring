73c8c6b9b3f4a73c93167d70b006cda140604b88
BenHalfon <Ben78901@gmail.com>
2022-05-19 13:51:25 +0300

    Renamed service to AutomationService

---
+++ src/main/java/com/tb/sh/netappautomationspring/bl/automation_services/VolumeAutomationService.java
@@ -0,0 +1,10 @@
+package com.tb.sh.netappautomationspring.bl.automation_services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class VolumeAutomationService {
+    private final Logger logger = Logger.getLogger(String.valueOf(VolumeAutomationService.class));
+}
